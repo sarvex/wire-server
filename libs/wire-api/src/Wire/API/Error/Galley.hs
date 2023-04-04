e9ce4b2f9284307b3c51b3bbd0f03420f8cdb882
Paolo Capriotti <paolo@capriotti.io>
2023-04-04 10:36:14 +0200

    Compute new node index for add proposals

--- libs/wire-api/src/Wire/API/Error/Galley.hs
+++ libs/wire-api/src/Wire/API/Error/Galley.hs
@@ -72,6 +72,7 @@ data GalleyError
   | MLSNonEmptyMemberList
   | MLSDuplicatePublicKey
   | MLSKeyPackageRefNotFound
+  | MLSInvalidLeafNodeIndex
   | MLSUnsupportedMessage
   | MLSProposalNotFound
   | MLSUnsupportedProposal
@@ -199,6 +200,8 @@ type instance MapError 'MLSNonEmptyMemberList = 'StaticError 400 "non-empty-memb
 type instance MapError 'MLSDuplicatePublicKey = 'StaticError 400 "mls-duplicate-public-key" "MLS public key for the given signature scheme already exists"
 
 type instance MapError 'MLSKeyPackageRefNotFound = 'StaticError 404 "mls-key-package-ref-not-found" "A referenced key package could not be mapped to a known client"
+
+type instance MapError 'MLSInvalidLeafNodeIndex = 'StaticError 400 "mls-invalid-leaf-node-index" "A referenced leaf node index points to a black or non-existing node"
 
 type instance MapError 'MLSUnsupportedMessage = 'StaticError 422 "mls-unsupported-message" "Attempted to send a message with an unsupported combination of content type and wire format"
 
